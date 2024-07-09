e79aefeb6ad77d85edf0c37eba0edcc647df90fb
공대베짱이 <dejavuhyo@gmail.com>
2024-07-09 13:25:23 +0900

    docs: update method

--- src/main/java/com/example/kafkaproducer/controller/KafkaProducerController.java
+++ src/main/java/com/example/kafkaproducer/controller/KafkaProducerController.java
@@ -4,8 +4,8 @@ import com.example.kafkaproducer.service.KafkaProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +16,7 @@ public class KafkaProducerController {
     private final KafkaProducerService kafkaProducerService;
 
     @PostMapping
-    public ResponseEntity<Void> sendMassage(@RequestParam String message) {
+    public ResponseEntity<Void> sendMassage(@RequestBody String message) {
         this.kafkaProducerService.sendMessage(message);
         return ResponseEntity.ok().build();
     }
